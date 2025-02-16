4f6f5de12cce3e48fb8d782afaefc10ea429880a
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-15 22:25:48 -0500

    start implementing DoublyLinkedList

--- dsa/DoublyLinkedList.js
+++ dsa/DoublyLinkedList.js
@@ -16,13 +16,18 @@ class DoublyLinkedList {
 
   // push()
   push(value) {
+    // Create a new node
     const newNode = new Node(value);
+    // If the list is empty, set both head and tail to the new node
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
+      // If the list is not empty, link the current tail's `next` to the new node
       this.tail.next = newNode;
+      // Set the new node's `previous` pointer to point back to the current tail
       newNode.previous = this.tail;
+      // Update the tail to be the new node
       this.tail = newNode;
     }
     this.length++;
