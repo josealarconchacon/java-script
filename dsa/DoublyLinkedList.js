d4fe1f1b36a9cf2bd7de4f90cba8a6729754eb66
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-15 22:23:15 -0500

    start implementing DoublyLinkedList

---
+++ dsa/DoublyLinkedList.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  // push()
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let doublyLinkedList = new DoublyLinkedList(1);
+// console.log(doublyLinkedList);
+doublyLinkedList.push(2);
+doublyLinkedList.push(3);
+console.log(doublyLinkedList);
